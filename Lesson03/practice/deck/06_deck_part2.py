c076d990f98105141073f90b8ca5b387f66a3a0d
boo <booblegum42@gmail.com>
2025-05-26 16:38:55 +0700

    Lesson03 deck part 6,7

--- Lesson03/practice/deck/06_deck_part2.py
+++ Lesson03/practice/deck/06_deck_part2.py
@@ -1,6 +1,22 @@
+import random
+
+
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
-    ...
+    def __init__(self, value: str, suit: str):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self) -> str:
+        suits_icons = {
+            'Diamonds': '\u2666',
+            'Hearts': '\u2665',
+            'Spades': '\u2660',
+            'Clubs': '\u2663',
+        }
+        return f"{self.value}{suits_icons[self.suit]}"
+
+    def equal_suit(self, other_card) -> bool:
+        return self.suit == other_card.suit
 
 
 class Deck:
@@ -9,20 +25,20 @@ class Deck:
         values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
         suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
         self.cards = []
-        # TODO-0: конструктор копируем из предыдущей задачи
+        for suit in suits:
+            for value in values:
+                self.cards.append(Card(value, suit))
 
-    def show(self):
-        # TODO-0: копируем из предыдущей задачи
-        ...
+    def show(self) -> str:
+        return f"cards[{len(self.cards)}]" + ", ".join([card.to_str() for card in self.cards])
 
-    def draw(self, x):
-        # TODO-1: Принцип работы данного метода прописан в 00_task_deck.md
-        ...
+    def draw(self, x: int) -> list[Card]:
+        cards = self.cards[:x]
+        self.cards = self.cards[x:]
+        return cards
 
-    def shuffle(self):
-        # TODO-2: Принцип работы данного метода прописан в 00_task_deck.md
-        #   Подсказка: https://www.w3schools.com/python/ref_random_shuffle.asp
-        ...
+    def shuffle(self) -> None:
+        random.shuffle(self.cards)
 
 
 # Создаем колоду
@@ -38,4 +54,4 @@ hand = deck.draw(5)
 # Выводим колоду, чтобы убедиться что 5 верхних карт отсутствуют
 print(deck.show())
 # Выводим список карт "в руке"(список hand)
-print(...)
+print(f"hand: {[card.to_str() for card in hand]}")
--- Lesson03/practice/deck/07_deck_part3.py
+++ Lesson03/practice/deck/07_deck_part3.py
@@ -1,18 +1,62 @@
+import random
+
+
 class Card:
-    # TODO-0: сюда копируем реализацию класса карты из предыдущего задания
+    def __init__(self, value: str, suit: str):
+        self.value = value  # Значение карты(2, 3... 10, J, Q, K, A)
+        self.suit = suit  # Масть карты
+
+    def to_str(self) -> str:
+        suits_icons = {
+            'Diamonds': '\u2666',
+            'Hearts': '\u2665',
+            'Spades': '\u2660',
+            'Clubs': '\u2663',
+        }
+        return f"{self.value}{suits_icons[self.suit]}"
+
+    def equal_suit(self, other_card) -> bool:
+        return self.suit == other_card.suit
 
     # TODO-1: реализуем новые методы
-    def more(self, other_card):
-        ...
+    def more(self, other_card) -> bool:
+        index_value1 = Deck.values.index(self.value)
+        index_value2 = Deck.values.index(other_card.value)
+        if index_value1 > index_value2:
+            return True
+        elif index_value1 < index_value2:
+            return False
+        else: # если значения равны
+            index_suit1 = Deck.suits.index(self.suit)
+            index_suit2 = Deck.suits.index(other_card.suit)
+            return index_suit1 > index_suit2
 
-    def less(self, other_card):
-        ...
 
+    def less(self, other_card) -> bool:
+        return not self.more(other_card)
 
 class Deck:
-    # TODO-0: сюда копируем реализацию класса колоды из предыдущего задания
-    ...
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    suits = ["Spades", "Clubs", "Diamonds", "Hearts"]
 
+    def __init__(self):
+        # Список карт в колоде. Каждым элементом списка будет объект класса Card
+
+        self.cards = []
+        for suit in self.suits:
+            for value in self.values:
+                self.cards.append(Card(value, suit))
+
+    def show(self) -> str:
+        return f"cards[{len(self.cards)}]" + ", ".join([card.to_str() for card in self.cards])
+
+    def draw(self, x: int) -> list[Card]:
+        cards = self.cards[:x]
+        self.cards = self.cards[x:]
+        return cards
+
+    def shuffle(self) -> None:
+        random.shuffle(self.cards)
 
 # Создаем колоду
 deck = Deck()
@@ -20,10 +64,14 @@ deck = Deck()
 deck.shuffle()
 print(deck.show())
 # Берем две карты из колоды
-card1, card2 = deck.draw(2)
+# card1, card2 = deck.draw(2)
+card1 = Card("K", "Hearts")
+card2 = Card("K", "Diamonds")
 
 # Тестируем методы .less() и .more()
-if card1.more(card2):
-    print(f"{card1.to_str()} больше {card2.to_str()}")
 if card1.less(card2):
-    print(f"{card1.to_str()} меньше {card2.to_str()}")
+    print(f"{card1.to_str()} меньше  {card2.to_str()}")
+else:
+    print(f"{card1.to_str()} больше {card2.to_str()}")
+# if card1.less(card2):
+#     print(f"{card1.to_str()} меньше {card2.to_str()}")
