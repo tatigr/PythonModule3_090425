30891ecfbb7aea552d2465f4b6a81148e8eb61e1
boo <booblegum42@gmail.com>
2025-06-04 16:51:09 +0700

    Lesson09 part1 complete

--- Lesson09/practice/regexp/02_task_regexp.py
+++ Lesson09/practice/regexp/02_task_regexp.py
@@ -1,5 +1,11 @@
 # Дано произвольное предложение. Слова разделены пробелами. Предложение содержит знаки препинания.
 # Найти все слова "the" в тексте, игнорируя регистр и убедившись,
 # что это именно слово, а не часть другого слова (например, "there").
+import re
 
 text_modified = "The quick brown fox jumps over there, near the lazy dog. THE end."
+template = r"\bthe\b"
+
+result = re.findall(template, text_modified, re.IGNORECASE)
+
+print(result)
--- Lesson09/practice/regexp/03_task_regexp.py
+++ Lesson09/practice/regexp/03_task_regexp.py
@@ -5,5 +5,21 @@
 # 3) все слова начинающиеся на гласную кириллическую букву
 # 4) все слова начинающиеся на согласную кириллическую букву
 # 5) все уникальные(без дубликатов) знаки препинания (знаками препинания считать символы .,!?;:)
+import re
 
-text = ...
+text = "Sed vestibulum laoreet a tellus; I vitae tincidunt odio euismod id. Nam!"
+
+# 1) первое слово из строки
+# template_task1 = r"^\w+\b"
+# first_word = re.match(template_task1, text)
+# print(first_word)
+
+# 2) первые два символа каждого слова
+template_task2 = r"\b\w{2}"
+two_chars = re.findall(template_task2, text)
+print(two_chars)
+# import string
+# 4) все слова начинающиеся на согласную латинскую букву
+template_task4 = r"\b[BCDFGHJKLMNPQRSTVWXYZ]\w*"
+words_consonant = re.findall(template_task4, text, re.IGNORECASE)
+print(words_consonant)
