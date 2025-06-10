5b2827801e382704b99174b2469f0b21b097f2fa
boo <booblegum42@gmail.com>
2025-06-10 16:03:56 +0700

    Lesson12 part2 complete

--- Lesson12/practice/TaskManager/main.py
+++ Lesson12/practice/TaskManager/main.py
@@ -9,8 +9,8 @@ class Task:
         COMPLETED: 'Завершено',
     }
     # DRY
-    def __init__(self, title, description="", status="Pending", priority=3):
-        self.id = ...
+    def __init__(self,id, title, description="", status="Pending", priority=3):
+        self.id = id
         self.title = title
         self.description = description
         self.status = status
@@ -29,24 +29,28 @@ class Task:
 
 
     def __repr__(self):
-        return f"Task: {self.title} status: {self.STATUSES_LOC[self.__status]}"
+        return f"Task[{self.id}]: {self.title} status: {self.STATUSES_LOC[self.__status]}"
 
 
 class TaskManager:
     def __init__(self):
         self._tasks = []
+        self._next_task_id = 1
 
-    def add_task(self, new_task: Task):
-        ...
+    def add_task(self, title, description="", status="Pending", priority=3):
+        new_task = Task(self._next_task_id, title, description, status, priority)
+        self._next_task_id += 1
+        self._tasks.append(new_task)
 
     def view_tasks(self) -> None:
-        ...
+        for task in self._tasks:
+            print(task)
 
 
 if __name__ == "__main__":
-    # task_manager = TaskManager()
-    task1 = Task("Купить продукты", "Молоко, хлеб, сыр...", status="Bad")
-    task1.status = Task.COMPLETED
-    print(task1)
-    # task1.status = Task.COMPLETED
-    # task_manager.add_task(task1)
+    task_manager = TaskManager()
+    task_manager.add_task("Купить продукты", "Молоко, хлеб, сыр...")
+    task_manager.add_task("Проверить работу класс", "Тестирование кода")
+
+    task_manager.view_tasks()
+
